49a886926d316ef29614a45128856310c40e6d79
Andrea Mostosi <andrea.mostosi@zenkay.net>
2014-07-16 18:48:11 +0200

    Basic structure of lib

---
+++ src/crate.php
@@ -0,0 +1,12 @@
+<?php
+
+include("lib/crate.client.php");
+include("lib/crate.error.php");
+include("lib/crate.result_set.php");
+include("lib/create.version.php");
+
+class Crate {
+  public function __construct() {
+
+  }
+}
